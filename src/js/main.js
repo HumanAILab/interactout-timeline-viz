9e0ca10a8e56a0f37c9192161ff7b39b74bcd0cf
Waley <waleyz@umich.edu>
2023-01-30 03:32:28 -0500

    Set up vis-timeline

--- src/js/main.js
+++ src/js/main.js
@@ -33,3 +33,24 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
+
+
+import { Timeline, DataSet } from "vis-timeline/standalone";
+// DOM element where the Timeline will be attached
+var container = document.getElementById('visualization');
+
+// Create a DataSet (allows two way data-binding)
+var items = new DataSet([
+  {id: 1, content: 'item 1', start: '2014-04-20'},
+  {id: 2, content: 'item 2', start: '2014-04-14'},
+  {id: 3, content: 'item 3', start: '2014-04-18'},
+  {id: 4, content: 'item 4', start: '2014-04-16', end: '2014-04-19'},
+  {id: 5, content: 'item 5', start: '2014-04-25'},
+  {id: 6, content: 'item 6', start: '2014-04-27', type: 'point'}
+]);
+
+// Configuration for the Timeline
+var options = {};
+
+// Create a Timeline
+var timeline = new Timeline(container, items, options);
